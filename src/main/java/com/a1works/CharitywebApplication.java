14ac877b3ccc66eecbd81037f7b90d9e71d08a65
Ahmed Magdy <Ahmed_Magdy@epam.com>
2016-09-19 16:46:46 +0200

    initial commit

---
+++ src/main/java/com/a1works/CharitywebApplication.java
@@ -0,0 +1,12 @@
+package com.a1works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitywebApplication.class, args);
+	}
+}
